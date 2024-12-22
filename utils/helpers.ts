86de418ab34fd296b1eac3000185afd67ca52e32
Sabra Funderburg <sabe.fun@gmail.com>
2024-12-22 16:25:44 -0600

    Updated status ID references

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,21 @@
+function getReceivedStatusId() {
+    return 1
+};
 
+function getAwaitingInfoStatusId() {
+    return 3
+};
+
+function getExtendedStatusId() {
+    return 5
+};
+
+function getMailedStatusId() {
+    return 6
+};
 
 function getCompletedStatusId() {
     return 10
-}
+};
 
-export {getCompletedStatusId}
+export  {getReceivedStatusId, getAwaitingInfoStatusId, getExtendedStatusId, getMailedStatusId, getCompletedStatusId}
