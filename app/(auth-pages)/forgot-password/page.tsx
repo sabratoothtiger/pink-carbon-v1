0130f5410f1f706fec5abaab125be2a747d5f161
Sabra Funderburg <sabe.fun@gmail.com>
2024-12-26 15:45:33 -0600

    Fix ForgotPassword function

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -5,11 +5,11 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-export default function ForgotPassword({
-  searchParams,
-}: {
-  searchParams: Message;
-}) {
+interface ForgotPasswordProps {
+  searchParams: { [key: string]: string | string[] };
+}
+
+export default function ForgotPassword({searchParams}:ForgotPasswordProps) {
   return (
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
