4a95b0e5f96edf06168d3b2de1ddaeeb66eab600
Sabra Funderburg <sabe.fun@gmail.com>
2024-12-29 19:30:43 -0600

    Fixed website reference

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial, sans-serif' }}>
             <h1>Uh oh! Looks like you may have gotten lost.</h1>
-            <p>Try <Link href="https://jmfcpallc.pinkcarbon.com">going back home</Link> for now.
+            <p>Try <Link href="https://jmfcpallc.pinkcarbon.app">going back home</Link> for now.
             </p>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Index() {
     <>
       <main className="flex-1 flex flex-col gap-6 px-4 mt-10">
         <div className="flex justify-center">
-          {!hasEnvVars && <EnvVarWarning />}
+          {hasEnvVars && <EnvVarWarning />}
         </div>
         <Header />
         <Hero />
