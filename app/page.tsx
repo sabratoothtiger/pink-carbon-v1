f9588e7126b9e4d58a2139e63c335b703b7b8c07
Sabra Funderburg <sabe.fun@gmail.com>
2024-12-29 18:29:22 -0600

    Fixed info banner spacing

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ import Hero from "@/components/home-page/hero";
 import Footer from "@/components/home-page/footer";
 import Header from "@/components/home-page/header";
 
-
 export default async function Index() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4 mt-10">
         <div className="flex justify-center">
-          {!hasEnvVars ? <EnvVarWarning /> : <></>}
+          {!hasEnvVars && <EnvVarWarning />}
         </div>
         <Header />
         <Hero />
