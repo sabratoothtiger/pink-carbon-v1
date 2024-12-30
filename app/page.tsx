e05e7ffd56ae6c215b9b4a8cf9ab19645b1f1bef
Sabra Funderburg <sabe.fun@gmail.com>
2024-12-30 12:22:49 -0600

    Remove top margin on home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Header from "@/components/home-page/header";
 export default async function Index() {
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4 mt-10">
+      <main className="flex-1 flex flex-col gap-6 px-4">
         <div className="flex justify-center">
           {!hasEnvVars ? <EnvVarWarning /> : <></>}
         </div>
